fdfb53237212d438cccf3b50ce039db86c083fb0
Mon6 <moises.castrejon.mendez@outlook.com>
2021-11-09 16:02:09 -0600

    codigo estable

--- ejemplo.c
+++ ejemplo.c
@@ -3,10 +3,7 @@ int main()
 {
 printf("hello world");
 printf("CAMBUIOS EN GITHUB");
-<<<<<<< HEAD
 printf("cambios de master");
-=======
 printf("cambios en mi rama");
->>>>>>> mirama
 return 0;
 }

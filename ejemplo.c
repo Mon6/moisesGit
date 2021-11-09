398e75b104df2c2ebc21fbd4dbe04a2a5d5f8032
Mon6 <moises.castrejon.mendez@outlook.com>
2021-11-09 16:30:53 -0600

    cambios de mi rama 1 y 2

--- ejemplo.c
+++ ejemplo.c
@@ -1,11 +1,8 @@
 #include
-int main()
-{
-
-printf("HOLA MUNDO");
-printf("cambios en mi rama1");
-printf("cambios en mi rama2");
-printf("cambios2 en mi rama2");
-
-return 0;
-}
+	int main(){
+		printf("HOLA MUNDO");
+		printf("cambios en mi rama1");
+		printf("cambios en mi rama2");
+		printf("cambios2 en mi rama2");
+		return 0;
+	}
